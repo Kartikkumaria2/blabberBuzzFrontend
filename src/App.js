3b0ab219a53aed3960d15125154842743f4c58fd
kartikkumaria2 <kkumaria_be21@thapar.edu>
2024-01-07 23:56:44 +0530

    second

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path = '/' element = {<Register/>} />
         <Route path='/Register' element={<Register />} />
         <Route path='/Login' element={<Login />} />
         <Route path='/Chat' element={<Chat />} />
